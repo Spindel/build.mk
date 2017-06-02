16e1bb197d9d640324c43022db52ae3d46bdb834
Nicklas Lindgren <nili@modio.se>
2017-06-02 11:41:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+build.mk
+========
+
+Official repository for the build.mk include file. Copy it into your
+project.
---
+++ build.mk
@@ -0,0 +1,239 @@
+## By setting certain variables before including build.mk in your
+## makefile you can set up some commonly used make rules for building
+## docker images.
+
+## Variables with uppercase names are used as the public interface
+## for build.mk.
+##
+## Variables with lowercase names are considered private to the
+## including makefile.
+##
+## Variables having names starting with underscore are considered
+## private to build.mk.
+
+
+## The fallback default goal does nothing. Set the variable
+## .DEFAULT_GOAL to change the default goal for your makefile, or
+## specify a rule before including build.mk if that is appropriate.
+
+default:
+
+
+# Set V=1 to echo the make recipes. Recipes are always echoed for CI
+# builds.
+
+ifeq ($(CI),)
+ifneq ($(V),1)
+Q = @
+endif
+endif
+
+
+## Add your built files to the CLEANUP_FILES variable to have them
+## cleaned up by the clean goal.
+
+.PHONY: clean
+clean:
+	$(Q)rm -f $(CLEANUP_FILES)
+
+
+## Set the ARCHIVE_PREFIX variable to specify the path prefix used for
+## all created tar archives.
+
+# Remove leading and add one trailing slash
+_archive_prefix := $(patsubst %/,%,$(patsubst /%,%,$(ARCHIVE_PREFIX)))/
+
+
+######################################################################
+### Git source archive
+######################################################################
+
+## Set the SOURCE_ARCHIVE variable to a file name to create a rule
+## which will create a git archive with that name for the head
+## revision. The archive will also include submodules.
+##
+## The ARCHIVE_PREFIX variable will specify the prefix path for the
+## archive.
+
+ifneq ($(SOURCE_ARCHIVE),)
+
+CLEANUP_FILES += $(SOURCE_ARCHIVE)
+
+# The git ref file indicating the age of HEAD
+GIT_HEAD_REF := $(shell git rev-parse --symbolic-full-name HEAD)
+GIT_TOP_DIR := $(shell git rev-parse --show-toplevel)
+GIT_HEAD_REF_FILE := $(shell git rev-parse --git-path $(GIT_HEAD_REF))
+
+# Handle that older git versions output git-path results relative to
+# the git top dir instead of relative to cwd
+GIT_HEAD_REF_FILE := $(shell if [ -f $(GIT_HEAD_REF_FILE) ]; then \
+                               echo $(GIT_HEAD_REF_FILE); \
+                             else \
+                               echo $(GIT_TOP_DIR)/$(GIT_HEAD_REF_FILE); \
+                             fi)
+
+$(SOURCE_ARCHIVE): $(GIT_HEAD_REF_FILE)
+	$(Q)tmpdir=$$(mktemp -d submodules.XXXXX) && \
+	trap "rm -rf $$tmpdir" EXIT && \
+	(cd "$(GIT_TOP_DIR)" && \
+	 git archive \
+	   -o "$(CURDIR)/$@" \
+	   --prefix="$(_archive_prefix)" \
+	   HEAD . && \
+	 git submodule update --init && \
+	 git submodule --quiet foreach 'echo $$path' | while read path; do \
+	   (cd "$$path" && \
+	    git archive \
+	      -o "$(CURDIR)/$$tmpdir/submodule.tar" \
+	      --prefix="$(_archive_prefix)$$path/" \
+	      HEAD . && \
+	    tar --concatenate -f "$(CURDIR)/$@" "$(CURDIR)/$$tmpdir/submodule.tar"); \
+	 done)
+
+endif
+
+
+######################################################################
+### Node packages
+######################################################################
+
+## Use the variable $(NODE_MODULES) as a prerequisite to ensure node
+## modules are installed for a make rule. Node modules will be
+## installed with yarn.
+##
+## Set the variable PACKAGE_JSON, if the package.json file is not in
+## the top-level directory.
+
+NODE = node
+PACKAGE_JSON ?= package.json
+NODE_MODULES ?= $(dir $(PACKAGE_JSON))node_modules/.mark
+
+$(NODE_MODULES): $(PACKAGE_JSON)
+	$(Q)(cd $(dir $<) && yarn) && touch $@
+
+
+######################################################################
+### Compiled archive from source archive
+######################################################################
+
+## Set the COMPILED_ARCHIVE variable to a file name to create a rule
+## which will run the shell command specified by COMPILE_COMMAND in a
+## temporary directory where the SOURCE_ARCHIVE has been unpacked. The
+## directory will be packed again into COMPILED_ARCHIVE.
+##
+## The ARCHIVE_PREFIX variable will specify the prefix path for the
+## archive.
+
+ifneq ($(COMPILED_ARCHIVE),)
+
+CLEANUP_FILES += $(COMPILED_ARCHIVE)
+
+$(COMPILED_ARCHIVE): $(SOURCE_ARCHIVE)
+	$(Q)tmpdir=$$(mktemp -d compilation.XXXXX) && \
+	trap "rm -rf $$tmpdir" EXIT && \
+	(tar -C $$tmpdir -xf $(SOURCE_ARCHIVE) && \
+	 (cd $$tmpdir/$(_archive_prefix) && $(COMPILE_COMMAND)) && \
+	 tar -C $$tmpdir $(_archive_prefix) -cf $(COMPILED_ARCHIVE))
+
+endif
+
+
+######################################################################
+### Docker image
+######################################################################
+
+## Set the IMAGE_ARCHIVE variable to a file name to create a rule
+## which will build a docker image, and written to IMAGE_ARCHIVE with
+## docker save.
+##
+## The IMAGE_REPO and IMAGE_TAG_PREFIX should be set to specify how
+## the image should be tagged. GitLab CI variables also affect the
+## tag.
+##
+## Set IMAGE_DOCKERFILE to specify a non-default dockerfile path. The
+## default is Dockerfile in the current directory.
+##
+## If the docker image uses any built file, these should be added as a
+## dependency for IMAGE_ARCHIVE. For example, you need the rule:
+##
+##   $(IMAGE_ARCHIVE): $(SOURCE_ARCHIVE)
+##
+## if the Dockerfile uses the SOURCE_ARCHIVE.
+##
+## The build and save goals both create $(IMAGE_ARCHIVE).
+##
+## The load goal loads $(IMAGE_ARCHIVE) into the docker daemon.
+##
+## The publish goal expects the image to be loaded into the daemon. It
+## will re-tag it to the final tag and push the image.
+
+ifneq ($(IMAGE_ARCHIVE),)
+
+.PHONY: build save load publish
+
+IMAGE_DOCKERFILE ?= Dockerfile
+
+CLEANUP_FILES += $(IMAGE_ARCHIVE)
+
+# The branch or tag name for which project is built
+CI_COMMIT_REF_NAME ?= $(shell git rev-parse --abbrev-ref HEAD)
+CI_COMMIT_REF_NAME := $(subst /,_,$(CI_COMMIT_REF_NAME))
+CI_COMMIT_REF_NAME := $(subst \#,_,$(CI_COMMIT_REF_NAME))
+
+# The commit revision for which project is built
+CI_COMMIT_SHA ?= $(shell git rev-parse HEAD)
+
+# The unique id of the current pipeline that GitLab CI uses internally
+CI_PIPELINE_ID ?= no-pipeline
+
+# The unique id of runner being used
+HOST := $(shell uname -a)
+
+# Build timestamp
+DATE := $(shell date --iso=minutes)
+
+# URL
+CI_PROJECT_URL ?= http://localhost.localdomain/
+
+# Unique for this build
+LOCAL_TAG = $(IMAGE_REPO):$(IMAGE_TAG_PREFIX)-$(CI_PIPELINE_ID)
+
+# Final tag
+TAG = $(IMAGE_REPO):$(IMAGE_TAG_PREFIX)-$(CI_COMMIT_REF_NAME)
+
+$(IMAGE_ARCHIVE): $(IMAGE_DOCKERFILE)
+	$(Q)docker build --pull --no-cache \
+	  --file=$< \
+	  --build-arg=BRANCH="$(CI_COMMIT_REF_NAME)" \
+	  --build-arg=COMMIT="$(CI_COMMIT_SHA)" \
+	  --build-arg=URL="$(CI_PROJECT_URL)" \
+	  --build-arg=DATE="$(DATE)" \
+	  --build-arg=HOST="$(HOST)" \
+	  --tag=$(LOCAL_TAG) \
+	  .
+	$(Q)docker save $(LOCAL_TAG) > $@
+	$(Q)docker rmi $(LOCAL_TAG)
+
+build save: $(IMAGE_ARCHIVE)
+
+load:
+	$(Q)docker load < $(IMAGE_ARCHIVE)
+
+publish:
+	$(Q)docker tag $(LOCAL_TAG) $(TAG)
+	$(Q)docker rmi $(LOCAL_TAG)
+	$(Q)docker push $(TAG)
+
+endif
+
+
+######################################################################
+### Test sequence helpers
+######################################################################
+
+## To run a series of tests where any may fail without stopping the
+## make recipe, use $(RECORD_TEST_STATUS) after each command, and end
+## the rule with $(RETURN_TEST_STATUS)
+
+RECORD_TEST_STATUS = let "_result=_result|$$?";
+RETURN_TEST_STATUS = ! let _result;
